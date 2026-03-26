1c62c817cb89e2032e0e4b8006896c16bf86d33c
psygreg <psygreg@pm.me>
2026-03-25 21:27:55 -0300

    first version

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # sloth-bash
 
 Use bash like a sloth. Veeeery lazy.
+
+## One-liner install
+`curl -fsSL https://raw.githubusercontent.com/psygreg/sloth-bash/main/install.sh | bash`
+
+## Recommended usage
+- Install *[Starship](curl -sS https://starship.rs/install.sh | sh)* for the complete experience.
+- Copy **sloth-bash** to **`~/.sloth-bash`**
+- Copy **alias-list** to **`~/.alias-list`**
+- Copy **starship.toml** to **`~/.config/starship.toml`** (not required if you don't wanna use *Starship*)
+- Add **`source ~/.sloth-bash`** to the end of your `~/.bashrc` file
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+read -r "Wanna be a Sloth? (y/n) " answer
+if [[ "${answer,,}" != "y" ]]; then
+    echo "Ok, bye!"
+    exit 0
+fi
+sudo -v
+curl -sS https://starship.rs/install.sh | sh
+wget -O ~/.sloth-bash https://raw.githubusercontent.com/psygreg/sloth-bash/main/sloth-bash
+wget -O ~/.alias-list https://raw.githubusercontent.com/psygreg/sloth-bash/main/alias-list
+wget -O ~/.config/starship.toml https://raw.githubusercontent.com/psygreg/sloth-bash/main/starship.toml
+if [[ -f ~/.bashrc ]]; then
+    if ! grep -q "source ~/.sloth-bash" ~/.bashrc; then
+        echo "source ~/.sloth-bash" >> ~/.bashrc
+    fi
+else
+    if ! grep -q "source ~/.sloth-bash" ~/.profile; then
+        echo "source ~/.sloth-bash" >> ~/.profile
+    fi
+fi
